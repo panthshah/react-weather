d1c448d4e833a4aee6a13a630dafe8986b7484db
panthshah <panthshah1920@gmail.com>
2022-11-30 20:29:06 -0500

    Assignment-10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Dimensions, Stylesheet, ScrollView, ImageBackground, View} from "react-native";
+import Weather from "./components/weather";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{
+        backgroundImage: `url("https://i.postimg.cc/2ygdn1WR/pexels-pixabay-531767.jpg")`, width:'100%', height:'100%',
+      }} className="AppBody">
+        <h1 style={{fontSize: '40px',
+    color: 'rgb(255, 255, 255)', textAlign:'center'}} className="WeatherForecastMainHeading">Today's Weather</h1>
+      <Router>
+        <div className="wrapper">
+          <Link to='/weekly'><button style={{marginTop: '30px', marginBottom: '30px',}}>Weather By Location</button></Link>
+              </div>
+              <div className="row">
+                <Switch>
+                <Route path="/weekly" component={Weather} />
+                </Switch>
+        </div>
+      </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
